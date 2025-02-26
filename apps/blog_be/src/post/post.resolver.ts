b245ff0183b24c324d997cb0c94880f194bc3833
Yasowant <yasowant1998@gmail.com>
2025-02-26 12:23:53 +0530

    home page designe and post page and pagination  updated code

--- apps/blog_be/src/post/post.resolver.ts
+++ apps/blog_be/src/post/post.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Context } from '@nestjs/graphql';
+import { Resolver, Query, Context, Args, Int } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
 import { UseGuards } from '@nestjs/common';
@@ -7,11 +7,21 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
-  @UseGuards(JwtAuthGuard)
+
+  // @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'posts' })
-  findAll(@Context() context) {
+  findAll(
+    @Context() context,
+    @Args('skip', { nullable: true }) skip?: number,
+    @Args('take', { nullable: true }) take?: number,
+  ) {
     const user = context.req.user;
     console.log({ user });
-    return this.postService.findAll();
+    return this.postService.findAll({ skip, take });
+  }
+
+  @Query(() => Int, { name: 'postCount' })
+  count() {
+    return this.postService.count();
   }
 }
